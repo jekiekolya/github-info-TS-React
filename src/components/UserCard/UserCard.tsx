0ffbb73755b2cf4b515f04b0efcfb64397d6423e
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-12 22:56:59 +0200

    add tittle and avatar

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -6,17 +6,27 @@ import s from "./UserCard.module.scss";
 
 // Types
 import { LocalGithubUser } from "types";
+import { UserTitle } from "components/UserTitle";
 
 interface UserCardProps extends LocalGithubUser {}
 
-export const UserCard = (props: UserCardProps) => (
-  <div className={s.userCard}>
-    <div></div>
-
-    <UserStat
-      repos={props.repos}
-      followers={props.followers}
-      following={props.following}
-    />
-  </div>
-);
+export const UserCard = (props: UserCardProps) => {
+  return (
+    <div className={s.userCard}>
+      <img src={props.avatar} alt={props.name} className={s.avatar} />
+      <UserTitle
+        created={props.created}
+        login={props.login}
+        name={props.name}
+      />
+      <p className={`${s.bio}${!props.bio ? "" : ` ${s.empty}`}`}>
+        {props.bio || "This user has no bio"}
+      </p>
+      <UserStat
+        repos={props.repos}
+        followers={props.followers}
+        following={props.following}
+      />
+    </div>
+  );
+};
